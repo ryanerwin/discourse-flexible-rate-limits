13f04fc65f6ed86e279450709f4b289a4457715c
U-Momon-PC\Momon <muhlisbc@gmail.com>
2018-06-05 00:58:34 +0700

    Prototype for Rspec test

--- plugin.rb
+++ plugin.rb
@@ -11,6 +11,30 @@ add_admin_route "flexible_rate_limits.admin.nav_label", "flexible-rate-limits"
 
 after_initialize {
 
+  class ::FlexibleRateLimits
+
+    attr_reader :topic_limit, :category_group_name
+
+    def initialize(user, category_id)
+      category_groups = PluginStore.get("flexible_rate_limits", "category_groups")
+      return if !category_groups.present?
+      return if user.blank?
+
+      category_group = category_groups.find { |cg| cg["categories"].include?(category_id) }
+
+      return if category_group.blank?
+
+      @category_group_name = category_group["name"]
+
+      group_ids = user.groups.pluck(:id)
+
+      group = (category_group["groups"] || []).find { |g| group_ids.include?(g["id"]) }
+
+      @topic_limit = group ? group["topic_limit"] : category_group["topic_limit"]
+    end
+
+  end
+
   class ::Admin::FlexibleRateLimitsController < ApplicationController
 
     def index
@@ -46,6 +70,27 @@ after_initialize {
     scope "/admin/plugins/flexible-rate-limits", constraints: AdminConstraint.new do
       get ""       => "admin/flexible_rate_limits#index"
       post "save"  => "admin/flexible_rate_limits#save"
+    end
+  }
+
+  Topic.class_eval {
+    alias_method :orig_limit_topics_per_day, :limit_topics_per_day
+
+    def limit_topics_per_day
+      if user && user.new_user_posting_on_first_day?
+        limit_first_day_topics_per_day
+      else
+        if SiteSetting.flexible_rate_limits_enabled
+          frl = FlexibleRateLimits.new(self.user, self.category_id)
+          if frl.category_group_name
+            RateLimiter.new(self.user, "cg-#{frl.category_group_name}-per-day", frl.topic_limit, 1.day.to_i)
+          else
+            apply_per_day_rate_limit_for("topics", :max_topics_per_day)
+          end
+        else
+          apply_per_day_rate_limit_for("topics", :max_topics_per_day)
+        end
+      end
     end
   }
 }
--- spec/integration/flexible_rate_limits_spec.rb
+++ spec/integration/flexible_rate_limits_spec.rb
@@ -8,33 +8,56 @@ def ps_get()
   PluginStore.get("flexible_rate_limits", "category_groups")
 end
 
-def create_post(params)
-  get "/posts/", params: params, headers: {"X-Requested-With" => "XMLHttpRequest"}
+def gen_title(n)
+  "Actually craft beer synth kinfolk trust fund tacos #{n}"
 end
 
-def fabricate_group_users(num, username_prefix, group)
-  num.times.map do |n|
-    user = Fabricate(:user, username: [username_prefix, n].join)
-    Fabricate(:group_user, user: user, group: group)
-  end
+def gen_raw(n)
+  "Literally lomo vice normcore keytar twee sustainable ethical cold-pressed #{n}"
 end
 
-describe("Flexible rate limits") {
+def _create_post(user, topic_id, num, expected, raw = nil)
+  expect {
+    num.times do |n|
+      post = PostCreator.create!(user, { raw: (raw || gen_raw(n)), topic_id: topic_id, skip_jobs: true }) rescue nil
+      expect(post&.post_number || 0).to_not eq(1)
+    end
+  }.to change{Post.count}.by(expected)
+end
+
+def _create_topic(user, category_id, num, expected)
+  expect {
+    num.times do |n|
+      PostCreator.create!(user, { title: gen_title(n), raw: gen_raw(n), category: category_id, skip_jobs: true }) rescue nil
+    end
+  }.to change{Topic.count}.by(expected)
+end
+
+describe("flexible_rate_limits") {
   let(:group) { Fabricate(:group) }
+  let(:old_user) { Fabricate(:leader) }
   let(:category) { Fabricate(:category) }
+  let(:new_user) { Fabricate(:newuser) }
+  let(:other_user) { Fabricate(:leader) }
+  let!(:topic) {
+    topic = Fabricate(:topic, category_id: category.id)
+    Fabricate(:post, topic_id: topic.id).save!
+    topic
+  }
 
   before {
     RateLimiter.enable
+    RateLimiter.clear_all!
 
     setting = [
       {
-        name: "Chatty",
-        topic_limit: 1,
+        name: "chatty",
+        topic_limit: 2,
         post_limit: 3,
         groups: [
           {
             id: group.id,
-            topic_limit: 2,
+            topic_limit: 3,
             post_limit: 4
           }
         ],
@@ -43,34 +66,103 @@ describe("Flexible rate limits") {
     ]
     ps_set(setting)
 
-    # SiteSetting.unique_posts_mins = 0 # How many minutes before a user can make a post with the same content again
-    # SiteSetting.rate_limit_create_topic = 0 # After creating a topic, users must wait (n) seconds before creating another topic
-    # SiteSetting.rate_limit_create_post = 0 # After posting, users must wait (n) seconds before creating another post.
-    # SiteSetting.rate_limit_new_user_create_topic = 0 # After creating a topic, new users must wait (n) seconds before creating another topic.
-    # SiteSetting.rate_limit_new_user_create_post = 0 # After posting, new users must wait (n) seconds before creating another post.
-    # SiteSetting.max_topics_in_first_day = 0 
+    SiteSetting.unique_posts_mins = 0 # How many minutes before a user can make a post with the same content again
+    SiteSetting.rate_limit_create_topic = 0 # After creating a topic, users must wait (n) seconds before creating another topic
+    SiteSetting.rate_limit_create_post = 0 # After posting, users must wait (n) seconds before creating another post.
+    SiteSetting.rate_limit_new_user_create_topic = 0 # After creating a topic, new users must wait (n) seconds before creating another topic.
+    SiteSetting.rate_limit_new_user_create_post = 0 # After posting, new users must wait (n) seconds before creating another post.
+    SiteSetting.max_topics_per_day = 100 # Maximum number of topics a user can create per day
+    SiteSetting.max_topics_in_first_day = 100 # The maximum number of topics a user is allowed to create in the 24 hour period after creating their first post
+    SiteSetting.max_replies_in_first_day = 100 # The maximum number of replies a user is allowed to create in the 24 hour period after creating their first post
+    SiteSetting.newuser_max_replies_per_topic = 100 # Maximum number of replies a new user can make in a single topic until someone replies to them.
   }
 
   context("topic") {
 
-    context("user member of group") {
-      before {
-        log_in_user(member)
+    context("category assigned to a category group") {
+      context("user member of group") {
+
+        before {
+          group.add(old_user)
+          group.add(new_user)
+        }
+
+        context("new user") {
+
+          context("topic") {
+
+            it("should respect rate_limit_new_user_create_topic") {
+              SiteSetting.rate_limit_new_user_create_topic = 100
+              _create_topic(new_user, category.id, 4, 1)
+            }
+
+            it("should respect max_topics_in_first_day") {
+              SiteSetting.max_topics_in_first_day = 2
+              _create_topic(new_user, category.id, 5, 2)
+            }
+
+          }
+
+          context("post") {
+
+            it("should respect unique_posts_mins") {
+              raw = "Pug ennui yolo knausgaard locavore farm-to-table."
+              _create_post(new_user, topic.id, 1, 1, raw)
+              SiteSetting.unique_posts_mins = 10
+              _create_post(new_user, topic.id, 1, 1, raw)
+              _create_post(new_user, topic.id, 1, 0, raw)
+            }
+
+            it("should respect rate_limit_new_user_create_post") {
+              SiteSetting.rate_limit_new_user_create_post = 100
+              _create_post(new_user, topic.id, 5, 1)
+            }
+
+            it("should respect newuser_max_replies_per_topic") {
+              SiteSetting.newuser_max_replies_per_topic = 2
+              _create_post(new_user, topic.id, 6, 2)
+            }
+
+            it("should respect max_replies_in_first_day") {
+              SiteSetting.max_replies_in_first_day = 5
+              _create_post(new_user, topic.id, 10, 5)
+            }
+
+          }
+
+        }
+
+        context("old user") {
+
+          it("should respect rate_limit_create_topic") {
+            SiteSetting.rate_limit_create_topic = 100
+            _create_topic(old_user, category.id, 10, 1)
+          }
+
+          it("should use custom rate limit instead of max_topics_per_day") {
+            _create_topic(old_user, category.id, 10, 3)
+          }
+
+        }
+
       }
 
-      it("should use custom rate limit") {
-
+      context("old user but not member of group") {
+        it("should use category_group default topic_limit") {
+          expect(FlexibleRateLimits.new(other_user, category.id).topic_limit).to eq(2)
+          _create_topic(other_user, category.id, 3, 2)
+        }
       }
-
     }
 
-    context("user member of group") {
-
+    context("category not assigned to a category group") {
+      it("should use default topic limit") {
+        expect(FlexibleRateLimits.new(old_user, 0).category_group_name).to eq(nil)
+        SiteSetting.max_topics_per_day = 5
+        _create_topic(old_user, 0, 10, 5)
+      }
     }
 
   }
 
-  context("post") {
-
-  }
 }
